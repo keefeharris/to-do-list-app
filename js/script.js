426a543586f31bbfa239b415c90cda310ba741ec
keefeharris <keefeharrisjr@gmail.com>
2021-05-10 14:52:38 -0400

    Update script.js
    if applied, a value that is empty will have and alert and if it has a value then its added to the list

--- js/script.js
+++ js/script.js
@@ -5,4 +5,15 @@ function newItem() {
   //created varibale inputValue which is equal to the id input and gets the value
   li.append(inputValue);
   //we append the inputValue to li so that we can start a list
+
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    let list = $("#list");
+    list.append(li);
+  }
+  /*
+    if inputValue has no entry then the window will use that alert
+    otherwise the id list is selected from the dom and the li with the valueInput is added to the list
+  */
 }
