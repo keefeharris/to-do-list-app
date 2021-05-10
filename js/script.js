7e2211ce75a267abc56c98998b7ef8484f88aa80
keefeharris <keefeharrisjr@gmail.com>
2021-05-10 14:45:46 -0400

    Update script.js
    if applied, we append the inputValue to li so that we can start a list

--- js/script.js
+++ js/script.js
@@ -3,4 +3,6 @@ function newItem() {
   //create varible li for new element li
   let inputValue = $("#input").val();
   //created varibale inputValue which is equal to the id input and gets the value
+  li.append(inputValue);
+  //we append the inputValue to li so that we can start a list
 }
