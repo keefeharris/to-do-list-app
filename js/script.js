5a8aaffaef5552f9b9e544a21a8e2bb482f11556
keefeharris <keefeharrisjr@gmail.com>
2021-05-10 14:37:10 -0400

    Update script.js
    if applied, created variable inputValue which is equal to the id input and gets the value

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 function newItem() {
   let li = $("<li></li>");
   //create varible li for new element li
+  let inputValue = $("#input").val();
+  //created varibale inputValue which is equal to the id input and gets the value
 }
